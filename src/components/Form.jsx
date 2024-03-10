a48e419affc9e272b0235dceb40236c1b98fa3f8
Rafael Farfan <rafaeldanielfarfan@gmail.com>
2024-03-10 16:14:21 -0500

    first commit

---
+++ src/components/Form.jsx
@@ -0,0 +1,20 @@
+
+
+const Form = () => {
+  return (
+    <div className="md:w-1/2 lg:w-2/5">
+
+        <h2 className="font-black text-3xl text-center mt-5">Seguimiento Pacientes</h2>
+      <p className="text-lg mt-1 text-center">
+        Añadir paciente y {""}
+        <span className="text-indigo-600 font-bold ">Administrar</span>
+      </p>
+    </div>
+  )
+}
+
+export default Form
+
+
+
+
---
+++ src/components/PatientsList.jsx
@@ -0,0 +1,10 @@
+
+const PatientsList = () => {
+  return (
+    <div className="md:w-1/2 lg:w-3/5">
+      <h2 className="font-black text-3xl text-center mt-5">Listado pacientes</h2>
+    </div>
+  )
+}
+
+export default PatientsList
